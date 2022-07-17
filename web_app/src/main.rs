a589b91ddb58439a743931efab890ae129015a56
Stephen Newman <steve@newman.digital>
2022-07-17 09:44:58 +0100

    Prior to pg. 42 Structuring code

---
+++ web_app/src/main.rs
@@ -0,0 +1,57 @@
+use rand::prelude::*;
+use std::env;
+
+/// This function generates a float number using a number
+/// generator passed into the function.
+///
+/// # Arguments
+/// * generator (&mut ThreadRng): the random number
+/// generator to generate the random number
+///
+/// # Returns
+/// (f64): random number between 0 -> 10
+fn generate_float(generator: &mut ThreadRng) -> f64 {
+    let placeholder: f64 = generator.gen();
+    return placeholder * 10.0
+}
+
+/// This trait defines the struct to be a user.
+trait IsUser {
+    /// This function proclaims that the struct is a user.
+    ///
+    /// # Arguments
+    /// None
+    ///
+    /// # Returns
+    /// (bool) true if user, false if not
+    fn is_user() -> bool {
+        return true
+    }
+}
+
+/// This struct defines a user
+///
+/// # Attributes
+/// * name (String): the name of the user
+/// * age (i8): the age of the user
+struct User {
+    name: String,
+    age: i8
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path: &str = &args[0];
+
+    if path.contains("/debug/") {
+        println!("The development app is running");
+    }
+    else if path.contains("/release/") {
+        println!("The production app is running");
+    }
+
+    let mut rng: ThreadRng = rand::thread_rng();
+
+    let random_number = generate_float(&mut rng);
+    println!("{}", random_number);
+}
